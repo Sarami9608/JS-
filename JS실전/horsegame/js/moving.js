2628b56ffb4a1a5fbe722e8baee56091adad302f
YOUNG <youngjae9683@gmail.com>
2023-03-22 17:44:24 +0900

    230322 animation 추가

--- JS실전/horsegame/js/moving.js
+++ JS실전/horsegame/js/moving.js
@@ -2,6 +2,7 @@
 // 초기 설정을 따로 해야함 ------ css의 값을 바로 가져올 수 없다.
 let img = document.getElementById("horse")
 let now = 0
+let height = 200
 function moveLeft(){
     if (now > 1160){
         now = 1210
@@ -21,9 +22,34 @@ function moveRight(){
     img.style.right = String(now) + 'px'
     
 }
+const jump = ()=>{
+    console.log('jump function')
+    // animation 기능
+    img.animate([
+        {bottom: '200px'},
+        {bottom: '400px'},
+        {bottom: '200px'}
+    ],{duration : 700})
+}
 let left = document.getElementsByClassName('btn')[0] 
 
 left.addEventListener('click', moveLeft)
 
 let right = document.getElementsByClassName('btn')[1] 
 right.addEventListener('click', moveRight)
+
+
+// 키보드로 조작
+//  e ==> 이벤트 객체
+document.getElementsByTagName('body')[0].addEventListener('keydown',(e)=>{
+    console.log('keydown Event',e)
+    if(e.key ==='ArrowLeft'){
+        moveLeft()
+    }
+    else if(e.key ==='ArrowRight' ){moveRight()}
+    else if(e.key ==='ArrowUp'|| e.key === ' '){
+        jump()
+
+    }
+    else if(e.key ==='ArrowDown'){}
+})
